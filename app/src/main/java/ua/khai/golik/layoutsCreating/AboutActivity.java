2f23f88a46d8986435b624a8cd437e35fda7ce7d
Max Golik <maximko1312@gmail.com>
2018-05-06 14:24:14 +0300

    Added all classes of activities
    Adde all classes & maded some edits in manifest file

---
+++ app/src/main/java/ua/khai/golik/layoutsCreating/AboutActivity.java
@@ -0,0 +1,15 @@
+package ua.khai.golik.layoutsCreating;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ua.golik.khai.air_tickets_app.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activity);
+    }
+}
---
+++ app/src/main/java/ua/khai/golik/layoutsCreating/MyOrdersActivity.java
@@ -0,0 +1,15 @@
+package ua.khai.golik.layoutsCreating;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ua.golik.khai.air_tickets_app.R;
+
+public class MyOrdersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_orders_activity);
+    }
+}
