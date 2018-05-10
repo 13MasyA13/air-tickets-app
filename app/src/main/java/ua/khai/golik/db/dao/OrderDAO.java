a6a1dcc178b43e3b7da180051f0b988dc0de82bf
Max Golik <maximko1312@gmail.com>
2018-05-11 02:39:56 +0300

    OrderDBOperations beginning
    Added demo db operations & gradle have been rebuilded

--- app/src/main/java/ua/khai/golik/db/dao/OrderDAO.java
+++ app/src/main/java/ua/khai/golik/db/dao/OrderDAO.java
@@ -1,29 +1,86 @@
 package ua.khai.golik.db.dao;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabaseLockedException;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import ua.khai.golik.db.DBHelper;
 import ua.khai.golik.db.dao.interfaces.OrderDBOperations;
 import ua.khai.golik.entities.Order;
 
 public class OrderDAO implements OrderDBOperations{
 
+    DBHelper dbHelper;
+
+    // TODO insertNewOrder()
+
     @Override
     public boolean insertNewOrder(Order order) {
-        return false;
+
+        SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        if(contentValues.size() != 0){
+            return true;
+        } else{
+            return false;
+
+        }
     }
 
     @Override
     public Order getOrderByID(int id) {
-        return null;
+        SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + DBHelper.ORDERS_TABLE
+                + " WHERE " + DBHelper.ORDER_ID + " = " + id, null);
+        Order order = new Order();
+        // TODO get data from cursor
+        return order;
     }
 
     @Override
     public boolean deleteOrderByID(int id) {
-        return false;
+        SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("DELETE * FROM " + DBHelper.ORDERS_TABLE
+                + " WHERE " + DBHelper.ORDER_ID + " = " + id, null);
+        // TODO how to check is data have been deleted
+        return true;
     }
 
     @Override
     public List<Order> getAllOrders() {
+        SQLiteDatabase sqLiteDatabase = dbHelper.getWritableDatabase();
+        List<Order> listOfAllOrders = new ArrayList<>();
+        Cursor cursor = sqLiteDatabase.query(DBHelper.ORDERS_TABLE, null, null, null, null, null, null);
+
+        cursor.moveToFirst();
+
+        // TODO DB structure
+
+        int id;
+        Date firstDate;
+        Date secondDate;
+        String from;
+        String to;
+        int price;
+        int countOfChildren;
+        int coundOfAdults;
+        int countOfSeats;
+
+        if(cursor.moveToFirst()){
+            while (cursor.moveToNext()){
+                Order order =  new Order();
+            }
+        } else{
+            // TODO event error
+        }
         return null;
     }
 }
