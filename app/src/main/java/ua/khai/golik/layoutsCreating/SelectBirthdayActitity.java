7ecf04bd5ce3beebbf519e374419573dfb95852d
Max Golik <32962754+13MasyA13@users.noreply.github.com>
2018-05-26 14:06:25 +0300

    Date selecters
    All activities with CalendarView have methods to select the date

--- app/src/main/java/ua/khai/golik/layoutsCreating/SelectBirthdayActitity.java
+++ app/src/main/java/ua/khai/golik/layoutsCreating/SelectBirthdayActitity.java
@@ -5,32 +5,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.CalendarView;
 
 import ua.golik.khai.air_tickets_app.R;
 
-public class SecondDateActivity extends AppCompatActivity {
+public class SelectBirthdayActitity extends AppCompatActivity {
 
-    private static final String secondDateActivity = "Second Date Activity";
+    private static final String selectBirthdayActivity = "Birthday Activity";
 
     private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_date_activity);
-
-        calendarView = findViewById(R.id.secondDateCalendar);
+        setContentView(R.layout.select_birthday_activity);
 
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 String date = (dayOfMonth + 1) + "/" + month + "/" + year;
-                Log.d(secondDateActivity, date);
+                Log.d(selectBirthdayActivity, date);
 
-                Intent intent = new Intent(SecondDateActivity.this, MakeOrderActivity.class);
-                intent.putExtra("secondDate", date);
+                Intent intent = new Intent(SelectBirthdayActitity.this, RegisterActivity.class);
+                intent.putExtra("birthdayDate", date);
                 startActivity(intent);
             }
         });
