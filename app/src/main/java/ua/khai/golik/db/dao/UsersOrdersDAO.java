206c65cff3e4cd387bcfbed1fa9f75870ee5205a
Max Golik <32962754+13MasyA13@users.noreply.github.com>
2018-05-26 15:14:48 +0300

    Added new methods UsersOrdersDAO
    getOrdersByUserID()
    [SQL raw query]

--- app/src/main/java/ua/khai/golik/db/dao/UsersOrdersDAO.java
+++ app/src/main/java/ua/khai/golik/db/dao/UsersOrdersDAO.java
@@ -15,7 +15,7 @@ public class UsersOrdersDAO implements UsersOrdersDBOperations{
     DBHelper dbHelper;
 
     @Override
-    public List<Order> getOrdersByUserID(int id) {
+    public List<Order> getOrdersByUserID(int user_id) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
 
         List<Order> listOfOrders = new ArrayList<>();
@@ -23,6 +23,34 @@ public class UsersOrdersDAO implements UsersOrdersDBOperations{
 
         cursor.moveToFirst();
 
-        return null;
+        int id = cursor.getColumnIndex(DBHelper.ORDER_ID);
+        int firstDate = cursor.getColumnIndex(DBHelper.FIRST_DATE);
+        int secondDate = cursor.getColumnIndex(DBHelper.SECOND_DATE);
+        int from = cursor.getColumnIndex(DBHelper.FROM_PLACE);
+        int to = cursor.getColumnIndex(DBHelper.TO_PLACE);
+        int price = cursor.getColumnIndex(DBHelper.PRICE);
+        int countOfChildren = cursor.getColumnIndex(DBHelper.COUNT_OF_CHILDREN);
+        int countOfAdults = cursor.getColumnIndex(DBHelper.COUNT_OF_ADULTS);
+        int countOfSeats = cursor.getColumnIndex(DBHelper.CLASS_OF_SEATS);
+
+        if(cursor.moveToFirst()){
+            while(cursor.moveToNext()){
+                Order order = new Order();
+
+                order.setId(cursor.getInt(id));
+                order.setFirst_date(cursor.getString(firstDate));
+                order.setSecond_date(cursor.getString(secondDate));
+                order.setFrom_place(cursor.getString(from));
+                order.setTo_place(cursor.getString(to));
+                order.setPrice(cursor.getInt(price));
+                order.setCount_of_children(cursor.getInt(countOfChildren));
+                order.setCount_of_adults(cursor.getInt(countOfAdults));
+                order.setCount_of_seats(cursor.getInt(countOfSeats));
+
+                listOfOrders.add(order);
+            }
+        }
+
+        return listOfOrders;
     }
 }
