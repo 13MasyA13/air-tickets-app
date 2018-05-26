7e6a4dbd2ec5b90766986977373b78033099604d
Max Golik <32962754+13MasyA13@users.noreply.github.com>
2018-05-26 14:09:21 +0300

    Addition to Date Selectors

--- app/src/main/java/ua/khai/golik/layoutsCreating/FirstDateActivity.java
+++ app/src/main/java/ua/khai/golik/layoutsCreating/FirstDateActivity.java
@@ -1,22 +1,37 @@
 package ua.khai.golik.layoutsCreating;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
+import android.widget.CalendarView;
 
 import ua.golik.khai.air_tickets_app.R;
 
 public class FirstDateActivity extends AppCompatActivity {
 
+    private static final String firstDateActivity = "FirstDateActivity";
+
+    private CalendarView calendarView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_date_activity);
-    }
 
-    // TODO onConfirmClick() [first_date]
+        calendarView = findViewById(R.id.firstDateCalendar);
 
-    public void onConfirmClick(View view){
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String date = (dayOfMonth + 1) + "/" + month + "/" + year;
+                Log.d(firstDateActivity, date);
 
+                Intent intent = new Intent(FirstDateActivity.this, MakeOrderActivity.class);
+                intent.putExtra("firstDate", date);
+                startActivity(intent);
+            }
+        });
     }
 }
