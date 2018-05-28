02833a86a2a18c33347996cc3f7e7f4d623ac746
Max Golik <32962754+13MasyA13@users.noreply.github.com>
2018-05-28 19:48:44 +0300

    Testing registration

--- app/src/main/java/ua/khai/golik/layoutsCreating/RegisterActivity.java
+++ app/src/main/java/ua/khai/golik/layoutsCreating/RegisterActivity.java
@@ -1,26 +1,38 @@
 package ua.khai.golik.layoutsCreating;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import ua.golik.khai.air_tickets_app.R;
 import ua.khai.golik.bl.interfaces.Validators;
+import ua.khai.golik.db.DBHelper;
+import ua.khai.golik.db.dao.AbstractDAOFactory;
+import ua.khai.golik.db.dao.SQLiteDAOFactory;
+import ua.khai.golik.db.dao.UserDAO;
 import ua.khai.golik.entities.User;
 
 public class RegisterActivity extends AppCompatActivity implements Validators {
 
     private static final String registerActivity = "RegisterActivity";
 
+    private DBHelper dbHelper;
+
     private EditText fNameEditText, lNameEditText, loginEditText, passwordEditText,
     firstPhoneEditText, secondPhoneEditText, emailEditText;
-    private TextView dateTextView;
+
+    public static TextView dateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_activity);
+
+        dbHelper = new DBHelper(this);
 
         fNameEditText = findViewById(R.id.firstName);
         lNameEditText = findViewById(R.id.lastName);
@@ -33,7 +45,12 @@ public class RegisterActivity extends AppCompatActivity implements Validators {
         dateTextView = findViewById(R.id.birthdayLabel);
     }
 
-    public void registrationOnClick(){
+    public void selectBirthdayDateOnClick(View view){
+        Intent selectBirthday = new Intent(RegisterActivity.this, SelectBirthdayActitity.class);
+        startActivity(selectBirthday);
+    }
+
+    public void registrationOnClick(View view){
         String firstName = fNameEditText.getText().toString();
         String lastName = lNameEditText.getText().toString();
         String login = loginEditText.getText().toString();
@@ -49,6 +66,60 @@ public class RegisterActivity extends AppCompatActivity implements Validators {
         int action = registrationValidator(validateValues);
 
         switch (action){
+            case -1:
+                Toast.makeText(RegisterActivity.this, "User with the same login is already exists!", Toast.LENGTH_LONG);
+                break;
+            case 0:
+                User user = new User();
+
+                user.setFirst_name(firstName);
+                user.setLast_name(lastName);
+                user.setLogin(login);
+                user.setPassword(password);
+                user.setFirst_phone_number(firstPhone);
+
+                if(secondPhone.equals("") | secondPhone == null){
+                    user.setSecond_phone_number("");
+                } else{
+                    user.setFirst_phone_number(secondPhone);
+                }
+                user.setBirthDate(dateOfBirthday);
+                user.setEmail(email);
+
+                AbstractDAOFactory daoFactory = new SQLiteDAOFactory();
+                UserDAO userDAO = daoFactory.getUserDAO();
+                boolean b = userDAO.insertNewUser(dbHelper, user);
+
+                if(b == true){
+                    Toast.makeText(RegisterActivity.this, "You are successfully registered! Please login into your account!", Toast.LENGTH_LONG);
+                    Intent loginActivity = new Intent("main.logging");
+                    startActivity(loginActivity);
+                } else{
+                    Toast.makeText(RegisterActivity.this, "App Fatal Error!!!!!!!!", Toast.LENGTH_LONG);
+                }
+
+                break;
+            case 1:
+                Toast.makeText(RegisterActivity.this, "First name is too longer then 20 characters!", Toast.LENGTH_LONG);
+                break;
+            case 2:
+                Toast.makeText(RegisterActivity.this, "Last name is too longer then 20 characters!", Toast.LENGTH_LONG);
+                break;
+            case 3:
+                Toast.makeText(RegisterActivity.this, "Login is too longer then 15 characters!", Toast.LENGTH_LONG);
+                break;
+            case 4:
+                Toast.makeText(RegisterActivity.this, "Password is too longer then 10 characters!", Toast.LENGTH_LONG);
+                break;
+            case 5:
+                Toast.makeText(RegisterActivity.this, "Invalid phone format!", Toast.LENGTH_LONG);
+                break;
+            case 6:
+                Toast.makeText(RegisterActivity.this, "Invalid phone format!", Toast.LENGTH_LONG);
+                break;
+            case 7:
+                Toast.makeText(RegisterActivity.this, "Email is too longer then 30 characters!", Toast.LENGTH_LONG);
+                break;
 
         }
     }
@@ -64,7 +135,7 @@ public class RegisterActivity extends AppCompatActivity implements Validators {
             return 2;
         }
 
-        if(values[2].length() > 10){
+        if(values[2].length() > 15){
             return 3;
         }
 
@@ -72,7 +143,7 @@ public class RegisterActivity extends AppCompatActivity implements Validators {
             return 4;
         }
 
-        if(values[4].length() > 13){
+        if(values[4].length() > 13 | values[4].length() == 0){
             return 5;
         }
 
@@ -84,7 +155,13 @@ public class RegisterActivity extends AppCompatActivity implements Validators {
             return 7;
         }
 
-        // TODO check is login unique
+        AbstractDAOFactory daoFactory = new SQLiteDAOFactory();
+        UserDAO userDAO = daoFactory.getUserDAO();
+        boolean b = userDAO.checkForUniqueLogin(dbHelper, values[2]);
+
+        if(b == false){
+            return -1;
+        }
 
         return 0;
     }
