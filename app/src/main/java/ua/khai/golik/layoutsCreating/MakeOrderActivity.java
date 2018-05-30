3b18837522d5318c22fcaff4da6f8e8299a16203
Max Golik <32962754+13MasyA13@users.noreply.github.com>
2018-05-30 16:17:40 +0300

    Added SeekBars

--- app/src/main/java/ua/khai/golik/layoutsCreating/MakeOrderActivity.java
+++ app/src/main/java/ua/khai/golik/layoutsCreating/MakeOrderActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import ua.golik.khai.air_tickets_app.R;
 
@@ -11,10 +13,56 @@ public class MakeOrderActivity extends AppCompatActivity {
 
     private static final String makeOrderActivity = "MakeOrderActivity";
 
+    private SeekBar adultsSeekBar, childrenSeekBar;
+
+    private TextView adultsBarValue, childrenBarValue;
+
+    public int childrenBarCount, adultsBarCount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.make_order_activity);
+
+        adultsBarValue = findViewById(R.id.adultsBarValue);
+        childrenBarValue = findViewById(R.id.childrenBarValue);
+
+        adultsSeekBar = findViewById(R.id.adultsSeekBar);
+        childrenSeekBar = findViewById(R.id.childrenSeekBar);
+
+        adultsSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                adultsBarValue.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                adultsBarCount = adultsSeekBar.getProgress();
+            }
+        });
+
+        childrenSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                childrenBarValue.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                childrenBarCount = childrenSeekBar.getProgress();
+            }
+        });
     }
 
     public void firstDateClick(View view){
