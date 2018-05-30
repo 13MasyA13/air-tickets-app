af542547a5fa74194b9e70e8560a1e40d0cb46be
Max Golik <32962754+13MasyA13@users.noreply.github.com>
2018-05-30 07:34:08 +0300

    The same like in previous commit

--- app/src/main/java/ua/khai/golik/bl/interfaces/CountFinalOrderPrice.java
+++ app/src/main/java/ua/khai/golik/bl/interfaces/CountFinalOrderPrice.java
@@ -1,7 +1,5 @@
 package ua.khai.golik.bl.interfaces;
 
-import java.util.Date;
-
 import ua.khai.golik.entities.Order;
 
 public interface CountFinalOrderPrice {
@@ -9,4 +7,12 @@ public interface CountFinalOrderPrice {
     double countPrice(Order order);
 
     long countDays(int[] firstDateMas, int[] secondDateMas);
+
+    boolean classOfSeats(String classOfSeats);
+
+    int[] regexStringToDataMass(String data);
+
+    double countPriceForToPlace(String toPlace);
+
+    double countPlacePrice(String place);
 }
