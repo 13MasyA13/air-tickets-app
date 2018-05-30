40f8974d4e1ffae689b6bf86de1335f3d9893e44
Max Golik <32962754+13MasyA13@users.noreply.github.com>
2018-05-30 15:21:48 +0300

    Added db management activity

---
+++ app/src/main/java/ua/khai/golik/layoutsCreating/DBManagementActivity.java
@@ -0,0 +1,15 @@
+package ua.khai.golik.layoutsCreating;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import ua.golik.khai.air_tickets_app.R;
+
+public class DBManagementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.data_base_management_activity);
+    }
+}
