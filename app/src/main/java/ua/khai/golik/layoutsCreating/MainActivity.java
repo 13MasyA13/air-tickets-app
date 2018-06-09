2c56a45a745724141f3186cd9ecebd6100bdf74b
Max Golik <maximko1312@gmail.com>
2018-06-09 15:19:51 +0300

    Main activity has been updated

--- app/src/main/java/ua/khai/golik/layoutsCreating/MainActivity.java
+++ app/src/main/java/ua/khai/golik/layoutsCreating/MainActivity.java
@@ -16,12 +16,6 @@ public class MainActivity extends AppCompatActivity {
     private static final String mainActivity = "MainActivity";
 
     private TextView textView;
-    private EditText secretEditText;
-    private Button secretButton;
-
-    private static int clickNum;
-
-    private static final String secretKey = "h56z38a";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,37 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
 
         textView = findViewById(R.id.accessDBManagemant);
-        secretEditText = findViewById(R.id.secretKeyEditText);
-        secretButton = findViewById(R.id.secretButton);
-
-        secretButton.setVisibility(View.INVISIBLE);
-        secretEditText.setVisibility(View.INVISIBLE);
-
-        clickNum = 0;
     }
 
     public void goButtonClick(View view){
         Intent loggingActivity = new Intent("main.logging");
         startActivity(loggingActivity);
-    }
-
-    public void secretButtonOnClick(View view){
-        String alivalibleKey = secretEditText.getText().toString();
-        if(alivalibleKey.equals(secretKey)){
-            Toast.makeText(MainActivity.this, "Right secret key", Toast.LENGTH_LONG);
-            clickNum = 0;
-        } else{
-            Toast.makeText(MainActivity.this, "Invalid secret key", Toast.LENGTH_LONG);
-            clickNum = 0;
-        }
-    }
-
-    public void accessDBManagementOnClick(View view){
-        clickNum++;
-
-        if(clickNum % 5 == 0){
-            secretEditText.setVisibility(View.VISIBLE);
-            secretButton.setVisibility(View.VISIBLE);
-        }
     }
 }
